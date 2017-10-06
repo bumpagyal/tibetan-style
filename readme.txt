87ccdcb04df9adf764db7edec4e63f46fbd2de8d
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2017-10-06 19:59:32 +0800

    create Plugin

---
+++ readme.txt
@@ -0,0 +1,23 @@
+﻿=== Tibetan Style ===
+
+Plugin Name: Tibetan Style
+Tags: Tibetan,Tibetan font
+Donate link: http://www.tibetitw.com/
+Plugin URI: http://www.tibetitw.com/wordpress-plugin/tibetan-style/
+Contributors: tibetitw.com
+Requires at least: 1.1.0
+Tested up to: 1.1.1
+Stable tag: 1.1.0
+
+== Description ==
+This plugin solves the default Windows Tibetan font is too small problem. Unified use of "Qomolangma-Uchen Sarchen" font.
+
+== Changelog ==
+= 1.1.1 =
+* 取消了对svg格式藏文Webfont字体的支持
+= 1.1.0 =
+* 优化了藏文Webfont字体
+= 1.0.1 =
+* 增加了WordPress默认编辑器支持藏文
+= 1.0.0 =
+* 发布 Tibetan Style wordpress 插件
---
+++ tibetan-style.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Tibetan Style
+Plugin URI: http://www.tibetitw.com/wordpress-plugin/tibetan-style/
+Description: This plugin solves the default Windows Tibetan font is too small problem. Unified use of "Qomolangma-Uchen Sarchen" font.
+Author: tibetitw.com
+Version: 1.1.1
+Author URI: http://www.tibetitw.com/
+*/
+
+function load_custom_admin_styles() {
+    wp_register_style( 'tibetan-style', plugins_url('', __FILE__) . '/tibetan-font.css', false, '1.1.1' );
+    wp_enqueue_style( 'tibetan-style' );
+    $custom_inline_style = '*,#wpadminbar *{font-family: "Qomolangma-Uchen Sarchen",sans-serif;}';
+    wp_add_inline_style( 'tibetan-style', $custom_inline_style );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_admin_styles' );
+add_action( 'login_enqueue_scripts', 'load_custom_admin_styles' );
+
+// Add editor styles
+function tibetan_add_editor_styles() {
+    add_editor_style( plugins_url('', __FILE__) . '/tibetan-editor-style.css' );
+    $font_url = plugins_url('', __FILE__) . '/tibetan-font.css';
+    add_editor_style( str_replace( ',', '%2C', $font_url ) );
+}
+add_action( 'init', 'tibetan_add_editor_styles' );
+
+function load_custom_styles() {
+    wp_register_style( 'tibetan-style-page', plugins_url('', __FILE__) . '/tibetan-font.css', false, '1.1.1' );
+    wp_enqueue_style( 'tibetan-style-page' );
+    $custom_inline_style = '#wpadminbar *{font-family: "Qomolangma-Uchen Sarchen",sans-serif;}';
+    wp_add_inline_style( 'tibetan-style-page', $custom_inline_style );
+}
+add_action( 'wp_enqueue_scripts', 'load_custom_styles' );
