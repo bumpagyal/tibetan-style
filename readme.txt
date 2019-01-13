ad5f94a21dffdd2d4bf31c4c141d3e50d03d80ea
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2019-01-13 23:19:55 +0800

    修改文件

--- readme.txt
+++ readme.txt
@@ -5,9 +5,9 @@ Tags: Tibetan,Tibetan font
 Donate link: http://www.tibetitw.com/
 Plugin URI: https://github.com/bumpagyal/tibetan-style/
 Contributors: tibetitw.com
-Requires at least: 1.2.2
-Tested up to: 1.2.3
-Stable tag: 1.2.2
+Requires at least: 1.2.3
+Tested up to: 1.2.4
+Stable tag: 1.2.3
 
 == Description ==
 此插件解决了WordPress后台藏文字体不统一的问题，统一使用了"Noto Sans Tibetan"字体，并提供了Webfont文件。
--- tibetan-style.php
+++ tibetan-style.php
@@ -4,18 +4,26 @@ Plugin Name: Tibetan Style
 Plugin URI: https://github.com/bumpagyal/tibetan-style
 Description: This plugin solves the default Windows Tibetan font is too small problem. Unified use of "Noto Sans Tibetan" font.
 Author: tibetitw.com
-Version: 1.2.2
+Version: 1.2.3
 Author URI: https://www.tibetitw.com/
 */
 
 function load_plugin_scripts() {
-    wp_register_style( 'tibetan-style', plugins_url('/tibetan-style.css', __FILE__), false, '1.2.2' );
+    wp_register_style( 'tibetan-style', plugins_url('/tibetan-style.css', __FILE__), false, '1.2.3' );
     wp_enqueue_style( 'tibetan-style' );
 }
 function load_plugin_admin_scripts() {
-    wp_register_style( 'tibetan-editor-style', plugins_url('/tibetan-editor-style.css', __FILE__), false, '1.2.2' );
+    wp_register_style( 'tibetan-editor-style', plugins_url('/tibetan-editor-style.css', __FILE__), false, '1.2.3' );
     wp_enqueue_style( 'tibetan-editor-style' );
 }
+function tibetan_add_google_fonts() {
+	wp_enqueue_style( 'tibetan-google-fonts', '//fonts.googleapis.com/earlyaccess/notosanstibetan.css', false ); 
+}
+
 add_action( 'wp_enqueue_scripts', 'load_plugin_scripts' );
 add_action( 'admin_enqueue_scripts', 'load_plugin_admin_scripts' );
 add_action( 'login_enqueue_scripts', 'load_plugin_admin_scripts' );
+
+add_action( 'wp_enqueue_scripts', 'tibetan_add_google_fonts' );
+add_action( 'admin_enqueue_scripts', 'tibetan_add_google_fonts' );
+add_action( 'login_enqueue_scripts', 'tibetan_add_google_fonts' );
