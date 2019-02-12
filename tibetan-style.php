cb47f7471437bd6cafd7a3e7aec1d71683d528b9
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2019-02-12 13:56:20 +0800

    edited file.

--- tibetan-style.php
+++ tibetan-style.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tibetan Style
 Plugin URI: https://github.com/bumpagyal/tibetan-style
-Description: This plugin solves the default Windows Tibetan font is too small problem. Unified use of "Noto Sans Tibetan" font.
+Description: This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, "Noto Sans Tibetan" is used as the default font to display Tibetan, instead of "Microsoft Himalaya" font. If it is an operating system such as a Mac, the default font body of the system will be called.
 Author: tibetitw.com
 Version: 1.3.3
 Author URI: https://www.tibetitw.com/
