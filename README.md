169b7957036449d525c4f99c1bf0dffc0d50e45c
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2019-11-14 18:20:46 +0800

    Updated file

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
 # Tibetan-Style
 
-This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, "Qomolangma-Uchen Sarchen" is used as the default font to display Tibetan, instead of "Microsoft Himalaya" font. If it is an operating system such as a Mac, the default font body of the system will be called.
+This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, Jomolhari is used as the default font to display Tibetan.
 
-此插件为针对在 Windows 系统里 WordPress 后台默认藏文字体太小的问题，当用户未安装任何藏文字体时 "Qomolangma-Uchen Sarchen" 作为默认字体来显示藏文，替代了 "Microsoft Himalaya" 字体。如果是 Mac 等操作系统，则会调用系统默认的藏文字体。
+此插件为针对在 Windows 系统里 WordPress 后台默认藏文字体太小的问题，当用户未安装任何藏文字体时 Jomolhari 作为默认字体来显示藏文。
 
 ## 如何使用
-下载本项目后，无需解压直接在WordPress后台添加插件并启用即可。
+下载本项目后，无需解压直接在 WordPress 后台添加插件并启用即可。
 
 ## 前端页面调用
-启用此插件后，主题会自动调用本地 WebFonts 文件，只要在您的主题样式表文件 style.css 里 font-family 属性添加 "Qomolangma-Uchen Sarchen" 或者 "Qomolangma-Uchen Sarchung" 字体即可。
+启用此插件后，主题会自动调用 Google Fonts 文件，只要在您的主题样式表文件 style.css 里 font-family 属性添加 Jomolhari 字体即可。
 
 ### 例如
 ```css
-font-family: "Qomolangma-Uchen Sarchen", "Helvetica Neue", Helvetica, "Segoe UI", Arial, sans-serif;
-```
-
-### 或者
-```css
-font-family: "Qomolangma-Uchen Sarchung", Georgia, "Times New Roman", Times, serif;
+font-family: Jomolhari, Georgia, "Times New Roman", Times, serif;
 ```
 
 ## 协议
--- tibetan-style.php
+++ tibetan-style.php
@@ -2,24 +2,36 @@
 /*
 Plugin Name: Tibetan Style
 Plugin URI:  https://www.tibetitw.com/wordpress-plugin/tibetan-style/
-Description: This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, "Qomolangma-Uchen Sarchen" is used as the default font to display Tibetan, instead of "Microsoft Himalaya" font. If it is an operating system such as a Mac, the default font body of the system will be called.
-Version:     1.3.6
+Description: This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, Jomolhari is used as the default font to display Tibetan.
+Version:     2.0
 Author:      Bumpagyal
 Author URI:  https://www.tibetitw.com/
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-function load_plugin_scripts() {
-    wp_register_style( 'tibetan-style', plugins_url('css/tibetan-style.css', __FILE__) );
+// Google Fonts
+function add_google_fonts() {
+	wp_register_style('admin-custom-font-files', 'https://fonts.googleapis.com/css?family=Jomolhari&display=swap&subset=tibetan', false );
+	wp_enqueue_style('admin-custom-font-files');
+}
+add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+add_action( 'admin_enqueue_scripts', 'add_google_fonts' );
+add_action( 'login_enqueue_scripts', 'add_google_fonts' );
+add_action( 'enqueue_embed_scripts', 'add_google_fonts' );
+
+// WordPress Custom Font @ Admin Embed
+function admin_custom_font() {
+    wp_register_style( 'tibetan-style', plugin_dir_url(__FILE__) . 'css/tibetan-style.css', false, '' );
     wp_enqueue_style( 'tibetan-style' );
 }
-function load_plugin_admin_scripts() {
-    wp_register_style( 'tibetan-editor-style', plugins_url('css/tibetan-editor-style.css', __FILE__) );
+add_action( 'wp_enqueue_scripts', 'admin_custom_font' );
+add_action( 'enqueue_embed_scripts', 'admin_custom_font' );
+
+// WordPress Custom Font @ Admin Login Editor
+function admin_editor_font() {
+    wp_register_style( 'tibetan-editor-style', plugin_dir_url(__FILE__) . 'css/tibetan-editor-style.css', false, '' );
     wp_enqueue_style( 'tibetan-editor-style' );
 }
-
-add_action( 'wp_enqueue_scripts', 'load_plugin_scripts' );
-add_action( 'admin_enqueue_scripts', 'load_plugin_admin_scripts' );
-add_action( 'login_enqueue_scripts', 'load_plugin_admin_scripts' );
-add_action( 'enqueue_embed_scripts', 'load_plugin_scripts' );
+add_action( 'login_enqueue_scripts', 'admin_editor_font' );
+add_action( 'admin_enqueue_scripts', 'admin_editor_font' );
