70faa0695537bc89fdcd1f062a5c37b2f259c2b2
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2019-12-10 16:40:23 +0800

    Updated file
    "Noto Sans Tibetan" is used as the default font to display Tibetan on Windows.

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # Tibetan-Style
 
-This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, "Monlam UI" is used as the default font to display Tibetan text.
+This plugin is for the problem that WordPress default Tibetan font is too small on Windows. When the user does not install any Tibetan font, "Noto Sans Tibetan" is used as the default font to display Tibetan.
 
-此插件为针对在 Windows 系统里 WordPress 后台默认藏文字体太小的问题，当用户未安装任何藏文字体时 Monlam UI 作为默认字体来显示藏文。
+此插件为针对在Windows系统里WordPress后台默认藏文字体太小的问题，当用户未安装任何藏文字体时"Noto Sans Tibetan"作为默认字体来显示藏文。
 
 ## 如何使用
-下载本项目后，无需解压直接在 WordPress 后台添加插件并启用即可。
+下载本项目后，无需解压直接在WordPress后台添加插件并启用即可。
 
 ## 前端页面调用
-启用此插件后，主题会自动调用有字库提供的 Web fonts 文件，只要在您的主题样式表文件 style.css 里 font-family 属性添加 "Microsoft Himalaya" 字体即可。
-
-### 例如
 ```css
-font-family: "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Monlam UI", sans-serif;
+font-family: "Segoe UI","Helvetica Neue",Kokonor,Kailasa,"Tibetan Machine Uni","Noto Sans Tibetan",sans-serif;
 ```
 
 ## 协议
--- tibetan-style.php
+++ tibetan-style.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Tibetan Style
 Plugin URI:  https://www.tibetitw.com/wordpress-plugin/tibetan-style/
-Description: This plugin is for the problem that WordPress default Tibetan font is too small on Windows system. When the user does not install any Tibetan font, "Monlam UI" is used as the default font to display Tibetan text.
-Version:     2.2.1
+Description: This plugin is for the problem that WordPress default Tibetan font is too small on Windows. When the user does not install any Tibetan font, "Noto Sans Tibetan" is used as the default font to display Tibetan.
+Version:     2.2.2
 Author:      Bumpa G. Rebkong
 Author URI:  https://www.tibetitw.com/
 License:     GPL2
@@ -12,16 +12,36 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 // WordPress Custom Font @ Admin Embed
 function admin_custom_font() {
-    wp_register_style( 'tibetan-style', plugin_dir_url(__FILE__) . 'css/tibetan-style.css', false, '' );
+
+    if(preg_match("/(Win|WIN|Windows|win|winNT).*?([\d.]+)/",$_SERVER['HTTP_USER_AGENT'])){
+	    wp_register_style( 'tibetan-win-style', plugin_dir_url(__FILE__) . 'css/index-win.css', false, '' );
+	    wp_enqueue_style( 'tibetan-win-style');
+    }
+
+    wp_register_style( 'tibetan-style', plugin_dir_url(__FILE__) . 'css/base-style.css', false, '' );
     wp_enqueue_style( 'tibetan-style' );
+
+
 }
 add_action( 'wp_enqueue_scripts', 'admin_custom_font' );
 add_action( 'enqueue_embed_scripts', 'admin_custom_font' );
 
 // WordPress Custom Font @ Admin Login Editor
 function admin_editor_font() {
-    wp_register_style( 'tibetan-editor-style', plugin_dir_url(__FILE__) . 'css/tibetan-editor-style.css', false, '' );
+
+    if(preg_match("/(Win|WIN|Windows|win|winNT).*?([\d.]+)/",$_SERVER['HTTP_USER_AGENT'])){
+	    wp_register_style( 'tibetan-win-style', plugin_dir_url(__FILE__) . 'css/index-win.css', false, '' );
+	    wp_enqueue_style( 'tibetan-win-style');
+    }
+
+    wp_register_style( 'tibetan-editor-style', plugin_dir_url(__FILE__) . 'css/editor-style.css', false, '' );
     wp_enqueue_style( 'tibetan-editor-style' );
+
 }
 add_action( 'login_enqueue_scripts', 'admin_editor_font' );
 add_action( 'admin_enqueue_scripts', 'admin_editor_font' );
+
+
+
+
+
